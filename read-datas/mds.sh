07e32743dc585d08018ead0404f6414799a87ab7
xzx <xuzhenxing@canaan-creative.com>
2018-07-18 20:29:59 +0800

    Add check duplication mds

--- read-datas/mds.sh
+++ read-datas/mds.sh
@@ -3,6 +3,13 @@
 # Author June 2018 Zhenxing Xu <xuzhenxing@canaan-creative.com>
 #
 
+
+chk=`ps -ef | grep './mdssub.sh' | wc -l`
+if [ $chk -ge '2' ]; then
+    echo -e "\033[1;31m++++++++++++++++++++++++++++++  Duplication MDS  ++++++++++++++++++++++++++++++\033[0m"
+    exit
+fi
+
 # Get delay times
 time=`cat miner-options.conf | grep TIME | awk '{ print $2 }'`
 
